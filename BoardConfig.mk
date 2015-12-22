2c62ab64cbc95ebc2753067ac07e29050e3e7299
wolverine2k <nareshtechs@gmail.com>
2015-12-22 13:59:47 +0100

    Updates for LiquidSmooth ROM

---
+++ BoardConfig.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# inherit from APQ8064 common
+-include device/oppo/apq8064-common/BoardConfigCommon.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/oppo/find5/include
+
+TARGET_BOARD_INFO_FILE := device/oppo/find5/board-info.txt
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_x909_defconfig
+
+#GCC
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7-sm
+TARGET_GCC_VERSION_AND := 4.8-sm
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oppo/find5/bluetooth
+
+# CM  Hardware
+BOARD_HARDWARE_CLASS += device/oppo/find5/cmhw
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_CACHEIMAGE_PARTITION_SIZE := 536870912
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+
+# inherit from the proprietary version
+-include vendor/oppo/find5/BoardConfigVendor.mk
---
+++ liquid.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, device/oppo/find5/full_find5.mk)
+
+# Inherit some common CM stuff
+#$(call inherit-product, vendor/liquid/config/gsm.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := find5
+PRODUCT_NAME := liquid_find5
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := find5
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_PRODUCT=find5 \
+    TARGET_DEVICE=find5 \
+    BUILD_FINGERPRINT="6.0.1/LYZ28J/20151223:user/release-keys" \
+    PRIVATE_BUILD_DESC="wolverine2k 6.0.1 LYZ28J eng.root.20151223 release-keys"
